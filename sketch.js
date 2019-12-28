516959c5c9a3ba350c9ceefc52b9b7d6ebdb5bcb
Shraddha3273 <58220358+Shraddha3273@users.noreply.github.com>
2019-12-28 19:03:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var database,form,player,game,playerCount,gameState = 0,allPlayers,
+distance = 0,car1,car2,car3,car4,cars;
+
+function setup(){
+   
+    createCanvas(displayWidth - 20,displayHeight - 30);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    background("white");
+    if(playerCount === 4){
+        game.update(1);
+    }
+    if(gameState === 1){
+        clear();
+        game.play();
+    }
+}
+
+
+
+
+
+
+
